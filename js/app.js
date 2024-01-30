b2614caa2cdd3eb259405ce31af59f2fef37001a
Mateus <mateus.lidonis@gmail.com>
2024-01-30 20:50:59 -0300

    Started Funny Button

--- js/app.js
+++ js/app.js
@@ -1,26 +1,39 @@
+window.onload = adicionarEventoHover;
+
+function adicionarEventoHover() {
+  var botao = document.getElementById("botao");
+  botao.addEventListener("mouseenter", moveButton);
+}
+
+//----------------------------------------------------------------------------------------------------------------------
 // Seleciona elementos do DOM para exibir informações do filme
 const moviePoster = document.querySelector(".movie-poster");
 const movieName = document.querySelector(".movie-name");
 const movieDescription = document.querySelector(".movie-description");
 const movieRank = document.querySelector(".movie-rank");
 const getRandomMovieButton = document.querySelector(".find-movie");
+const divProviders = document.getElementById("provider-img");
 const language = "language=pt-BR";
-/*
-const movieYear = document.getElementById("movie-year");
-const yearInput = movieYear.value;*/
-
+//----------------------------------------------------------------------------------------------------------------------
+move = 0;
+//----------------------------------------------------------------------------------------------------------------------
 // Decifra a chave da API
 var shift = 3;
 var data = decrypt("67fig3162d3984fh291ggi2g017fge32", shift);
 const API_KEY = data;
+//----------------------------------------------------------------------------------------------------------------------
 
-// Função para buscar todos os gêneros disponíveis
+//----------------------------------------------------------------------------------------------------------------------
+// Função para buscar todos os gêneros disponíveis ao iniciar a página
 getGenres();
+//----------------------------------------------------------------------------------------------------------------------
 
-//------------------------------------------------------------------------------
-// Evento de clique para buscar um filme aleatório
+//----------------------------------------------------------------------------------------------------------------------
+//Evento de clique para buscar um filme aleatório
 getRandomMovieButton.addEventListener("click", async () => {
-  console.log("------------------------------------------------------------");
+  console.log("----------------BUTTON CLICK----------------");
+
+  //--------------------------------------------------------------------------------------------------------------------
   // Obtém os valores dos checkboxes marcados
   let checkboxes = document.querySelectorAll("input[type='checkbox']");
   let checkedIds = [];
@@ -29,74 +42,27 @@ getRandomMovieButton.addEventListener("click", async () => {
       checkedIds.push(checkbox.id);
     }
   });
-
-  const randomPage = await getPage(checkedIds);
   console.log("checkedIds: " + checkedIds);
-  console.log("Random Page: " + randomPage);
+  //--------------------------------------------------------------------------------------------------------------------
+  // Escolhe uma página aleatória de todas retornadas pela API
+  const randomPage = await getPage(checkedIds);
+  //--------------------------------------------------------------------------------------------------------------------
   // Exibe o contêiner do filme e desativa o botão
   document.getElementById("movie-container").style.display = "flex";
   getRandomMovieButton.style.opacity = "0.8";
   getRandomMovieButton.style.pointerEvents = "none";
-
-  const divProviders = document.getElementById("provider-img");
-  divProviders.innerHTML = "";
-
+  //--------------------------------------------------------------------------------------------------------------------
   // Busca um filme aleatório
-  //const movieData = await fetchMovieWithDescription(randomId);
   const movieData = await fetchMoviesWithGenres(checkedIds, randomPage);
-
-  console.log("----------------O QUE VAI APARECER----------------");
-
-  console.log("Movie Data: " + movieData);
-  console.log("Poster Path: " + movieData.poster_path);
-  console.log("Title: " + movieData.title);
-  console.log("Overview: " + movieData.overview);
-  console.log("Nota: " + movieData.vote_average);
-
-  // Exibe as informações do filme
-  moviePoster.src = `https://image.tmdb.org/t/p/w500${movieData.poster_path}`;
-  movieName.textContent = movieData.title;
-  movieDescription.textContent = movieData.overview;
-  movieRank.textContent = movieData.vote_average;
-
+  //--------------------------------------------------------------------------------------------------------------------
   // Reativa o botão
   getRandomMovieButton.style.opacity = "1";
   getRandomMovieButton.style.pointerEvents = "auto";
 });
-//------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------
 
-//------------------------------------------------------------------------------
-// Função para criptografar um texto
-function encrypt(text, shift) {
-  var result = "";
-  // Itera sobre cada caractere do texto
-  for (var i = 0; i < text.length; i++) {
-    // Itera sobre cada caractere do texto
-    var asciiCode = text.charCodeAt(i);
-    // Verifica se o caractere é uma letra maiúscula
-    if (asciiCode >= 65 && asciiCode <= 90) {
-      // Se for, criptografa o caractere e adiciona ao resultado
-      result += String.fromCharCode(((asciiCode - 65 + shift) % 26) + 65);
-      // Verifica se o caractere é uma letra minúscula
-    } else if (asciiCode >= 97 && asciiCode <= 122) {
-      // Se for, criptografa o caractere e adiciona ao resultado
-      result += String.fromCharCode(((asciiCode - 97 + shift) % 26) + 97);
-      // Se o caractere não for uma letra, adiciona ao resultado sem criptografar
-    } else {
-      result += text.charAt(i);
-    }
-  }
-  // Retorna o texto criptografado
-  return result;
-}
-// Função para descriptografar um texto
-function decrypt(text, shift) {
-  // Chama a função encrypt com 26 - shift para descriptografar o texto
-  return encrypt(text, 26 - shift);
-}
-//------------------------------------------------------------------------------
-
-//------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------
+// Função que retorna todos os gêneros disponíveis na API para exibí-los nas checkboxes
 function getGenres() {
   // Busca a lista de gêneros de filmes
   fetch(
@@ -140,21 +106,67 @@ function getGenres() {
 
   return null;
 }
-//------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------
 
-//------------------------------------------------------------------------------
-// Função para buscar um filme com descrição
+//----------------------------------------------------------------------------------------------------------------------
+async function getPage(checkedIds) {
+  const movie = await fetch(
+    `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&${language}&primary_release_year=2023&with_original_language=en&with_genres=${checkedIds}`
+  );
+  const movieData = await movie.json();
+  const totalPages = movieData.total_pages;
+  let randomPage = Math.floor(Math.random() * totalPages + 1);
+
+  console.log("----------------GET PAGE----------------");
+  console.log("RandomPage antes da verificação de 500 páginas: " + randomPage);
+  // A API tem um limite de 500 páginas (mesmo se retornarem > 500 em totalPages)
+  // Esse While faz com que, caso retorne algo maior que 500, o sistema procura uma nova página
+  while (randomPage > 500) {
+    randomPage = Math.floor(Math.random() * totalPages + 1);
+  }
+  console.log("Resposta do fetch para retornar página aleatória: ");
+  console.log(movie);
+  console.log("movieData: ");
+  console.log(movieData);
+  console.log("totalPages: " + totalPages);
+  console.log("randomPage: " + randomPage);
+  return randomPage;
+}
+//----------------------------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------------------------
+// Função para retornar um ID de um filme aleatório de uma página aleatória
+async function fetchMoviesWithGenres(checkedIds, randomPage) {
+  const movie = await fetch(
+    `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&${language}&with_genres=${checkedIds}&page=${randomPage}&sort_by=popularity.desc`
+  );
+  const movieData = await movie.json();
+  const RandomIndex = Math.floor(Math.random() * movieData.results.length);
+
+  console.log("----------------MOVIES WITH GENRES----------------");
+  console.log("Resposta do fetchMoviesWithGenres com Ids e randomPage: ");
+  console.log(movie);
+  console.log("movieData: ");
+  console.log(movieData);
+  console.log("randomIndex: " + RandomIndex);
+  console.log("Filme aleatório baseado no index: ");
+  console.log(movieData.results[RandomIndex]);
+
+  let idForMovieWithGenre = movieData.results[RandomIndex].id;
+  getMovieProvider(idForMovieWithGenre);
+  return fetchMovieWithDescription(idForMovieWithGenre);
+}
+//----------------------------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------------------------
+// Função para buscar um filme com descrição baseado no ID aleatório retornado por fetchMoviesWithGenres
 async function fetchMovieWithDescription(randomId) {
   const movie = await fetch(
     `https://api.themoviedb.org/3/movie/${randomId}?api_key=${API_KEY}&${language}`
-    /*`https://api.themoviedb.org/3/discover/movie/?api_key=${API_KEY}&with_genres=${checkedIds.join(
-      ","
-    )}&region=US`*/
   );
-  console.log("----------------MOVIE WITH DESCRIPTION----------------");
-
   const movieData = await movie.json();
-  // Verifica se o filme tem descrição
+
+  console.log("----------------MOVIE WITH DESCRIPTION----------------");
   console.log(
     "Descrição: " +
       movieData.overview +
@@ -168,83 +180,44 @@ async function fetchMovieWithDescription(randomId) {
       "Nota: " +
       movieData.vote_average
   );
+  // Se o filme tem descrição ...
   if (
     movieData.overview &&
     movieData.poster_path &&
     movieData.title &&
     movieData.vote_average
   ) {
-    // Se o filme tem descrição, retorna os dados do filme
-    return movieData;
+    // ... então imprime os dados do filme no HTML
+    moviePoster.src = `https://image.tmdb.org/t/p/w500${movieData.poster_path}`;
+    movieName.textContent = movieData.title;
+    movieDescription.textContent = movieData.overview;
+    movieRank.textContent = movieData.vote_average;
   } else {
-    // Se o filme não tem descrição, busca outro filme
-    /* const newRandomId = Math.floor(Math.random() * 500000);
-    return fetchMovieWithDescription(newRandomId);*/
-    return movieData;
+    // Se o filme não tem descrição
+    // Hoje não é dia de filme :)
+    moviePoster.src = "./assets/logo_without_movie.png";
+    movieName.textContent = "Sem filme hoje!";
+    movieDescription.textContent =
+      "Que tal uma boa leitura? Ou então terminar aquele projeto que você deixou parado?";
+    movieRank.textContent = "Mais de 8 mil!";
   }
 }
-//------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------
 
-//------------------------------------------------------------------------------
-async function fetchMoviesWithGenres(checkedIds, randomPage) {
-  const movie = await fetch(
-    `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&${language}&with_genres=${checkedIds}&page=${randomPage}&sort_by=popularity.desc`
-  );
-  console.log("----------------MOVIES WITH GENRES----------------");
-
-  const movieData = await movie.json();
-  const RandomIndex = Math.floor(Math.random() * movieData.results.length);
-  console.log("Resposta do fetchMoviesWithGenres com Ids e randomPage: ");
-  console.log(movie);
-  console.log("movieData: ");
-  console.log(movieData);
-  console.log("randomIndex: " + RandomIndex);
-  console.log("Filme aleatório baseado no index: ");
-  console.log(movieData.results[RandomIndex]);
-  let idForMovieWithGenre = movieData.results[RandomIndex].id;
-  getMovieProvider(idForMovieWithGenre);
-  return fetchMovieWithDescription(idForMovieWithGenre);
-}
-//------------------------------------------------------------------------------
-
-//------------------------------------------------------------------------------
-async function getPage(checkedIds) {
-  const movie = await fetch(
-    `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&${language}&primary_release_year=2023&with_original_language=en&with_genres=${checkedIds}`
-  );
-  console.log("----------------GET PAGE----------------");
-
-  const movieData = await movie.json();
-  const totalPages = movieData.total_pages;
-  let randomPage = Math.floor(Math.random() * totalPages + 1);
-  console.log("RandomPage antes: " + randomPage);
-  while (randomPage > 500) {
-    randomPage = Math.floor(Math.random() * totalPages + 1);
-  }
-  console.log("Resposta do fetch para retornar página aleatória: ");
-  console.log(movie);
-  console.log("movieData: ");
-  console.log(movieData);
-  console.log("totalPages: " + totalPages);
-  console.log("randomPage: " + randomPage);
-  return randomPage;
-}
-
-//------------------------------------------------------------------------------
-
-//------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------
 async function getMovieProvider(idForMovieWithGenre) {
   const movie = await fetch(
-    /*`https://api.themoviedb.org/3/watch/providers/movie?api_key=${API_KEY}&language=en-US`*/
     `https://api.themoviedb.org/3/movie/${idForMovieWithGenre}/watch/providers?api_key=${API_KEY}`
   );
   const movieData = await movie.json();
+  const result = movieData.results.BR;
+
   console.log("----------------PROVIDERS----------------");
   console.log(movieData);
-
-  const result = movieData.results.BR;
+  // Se não existir opções no Brasil
   if (!result) {
     console.log("Não existe");
+    // Se existir, então o índice vai ser igual ao tamanho (caso exista flatrate, buy ou rent), se não existir, indice = 0
   } else {
     var indexBuy = result.buy ? result.buy.length : 0;
     var indexRent = result.rent ? result.rent.length : 0;
@@ -257,7 +230,7 @@ async function getMovieProvider(idForMovieWithGenre) {
   let countFlatrate = 0;
   let countBuy = 0;
   let countRent = 0;
-  //console.log(movieData.results.BR.flatrate[0].provider_name);
+
   console.log("----------------STREAMING----------------");
   for (count = 0; countFlatrate < indexFlatrate; countFlatrate++) {
     console.log(
@@ -290,28 +263,11 @@ async function getMovieProvider(idForMovieWithGenre) {
         movieData.results.BR.rent[countRent].provider_id
     );
   }
-
-  /*const index = movieData.results.length;
-  console.log("----------------PROVIDERS----------------");
-  console.log(movieData);
-  let count = 0;
-  for (count = 0; count < index; count++) {
-    console.log(
-      "Nome: " +
-        movieData.results[count].provider_name +
-        " / " +
-        "Id: " +
-        movieData.results[count].provider_id
-    );
-  }*/
 }
-/*
-&with_watch_providers=337&watch_region=BR
-337 = Disney Plus
-8 = Netflix
-619 = Star Plus
-*/
-//------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------------------------
+// Função para mostrar os provedores de streaming, compra ou aluguel dos filmes
 function insertProviders(providers, type) {
   if (!providers) {
     console.error("Nenhum provedor fornecido");
@@ -331,4 +287,45 @@ function insertProviders(providers, type) {
     img.title = provider.provider_name;
     divProviders.appendChild(img);
   });
+}
+//----------------------------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------------------------
+// Função para criptografar um texto
+function encrypt(text, shift) {
+  var result = "";
+  // Itera sobre cada caractere do texto
+  for (var i = 0; i < text.length; i++) {
+    // Itera sobre cada caractere do texto
+    var asciiCode = text.charCodeAt(i);
+    // Verifica se o caractere é uma letra maiúscula
+    if (asciiCode >= 65 && asciiCode <= 90) {
+      // Se for, criptografa o caractere e adiciona ao resultado
+      result += String.fromCharCode(((asciiCode - 65 + shift) % 26) + 65);
+      // Verifica se o caractere é uma letra minúscula
+    } else if (asciiCode >= 97 && asciiCode <= 122) {
+      // Se for, criptografa o caractere e adiciona ao resultado
+      result += String.fromCharCode(((asciiCode - 97 + shift) % 26) + 97);
+      // Se o caractere não for uma letra, adiciona ao resultado sem criptografar
+    } else {
+      result += text.charAt(i);
+    }
+  }
+  // Retorna o texto criptografado
+  return result;
+}
+// Função para descriptografar um texto
+function decrypt(text, shift) {
+  // Chama a função encrypt com 26 - shift para descriptografar o texto
+  return encrypt(text, 26 - shift);
+}
+//----------------------------------------------------------------------------------------------------------------------
+
+function moveButton() {
+  var rect = this.getBoundingClientRect();
+  var x = Math.floor(Math.random() * (window.innerWidth - rect.width));
+  var y = Math.floor(Math.random() * (window.innerHeight - rect.height));
+  this.style.left = x + "px";
+  this.style.top = y + "px";
+  this.style.position = "absolute";
 }
